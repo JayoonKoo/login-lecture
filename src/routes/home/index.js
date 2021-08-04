f1bc85ef90a200139bae95d3b87efe88d25a4a58
JayoonKoo <rnwkdbs12@gmail.com>
2021-08-05 01:42:28 +0900

    feat: create init

---
+++ src/routes/home/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+const {hello, login} = require("./home.ctrl");
+
+router.get("/", hello);
+
+router.get("/login", login);
+
+module.exports = router;
