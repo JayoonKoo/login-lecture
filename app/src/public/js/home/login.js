2d051c9fc880c44e6bf6d25953db6a04213e6a5d
JayoonKoo <rnwkdbs12@gmail.com>
2021-08-06 17:29:09 +0900

    feat: create login fetch
    로그인 요청시 발생하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,23 @@
 "use strict";
 
-const id = document.querySelector("")
+const id = document.querySelector("#id"),
+	password = document.querySelector("#password"),
+	button = document.querySelector("button");
+
+button.addEventListener("click", () => login());
+
+
+function login() {
+	const req = {
+		id: id.value,
+		password: password.value
+	};
+	
+	fetch("/login", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(req),
+	})
+}
